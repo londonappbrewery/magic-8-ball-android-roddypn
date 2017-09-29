3dab84ab56c66ab63c9aac39b2926912eb2542c0
RodriguePoon <poonrodrigue@gmail.com>
2017-09-30 00:26:41 +0800

    Project Completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
 
         final int[] ballArray = {
